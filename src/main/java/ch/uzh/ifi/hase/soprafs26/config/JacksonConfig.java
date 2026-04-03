72c9512aa8b54e4172d120a5574e7314df755e02
Claude Stark <125886240+ClaudeStark@users.noreply.github.com>
2026-04-03 15:28:27 +0200

    fix: added config for Bean ObjectMapper

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/config/JacksonConfig.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.hase.soprafs26.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+}
