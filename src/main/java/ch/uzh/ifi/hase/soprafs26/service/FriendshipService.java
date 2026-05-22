6d8653bcc035b1fde70fbf2ad760b4538bef5e74
Remo Weirich <182071194+remoweirich@users.noreply.github.com>
2026-05-22 14:37:28 +0200

    fix: still wrong user in feedback message

--- src/main/java/ch/uzh/ifi/hase/soprafs26/service/FriendshipService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/service/FriendshipService.java
@@ -60,8 +60,8 @@ public class FriendshipService {
         Message message = new Message(MessageType.FRIEND_REQUEST, friendRequestDTO);
         simpMessagingTemplate.convertAndSend("/topic/" + receivingUserId + "/notifications", message);
 
-        friendRequestDTO.setUserId(sendingUserId);
-        friendRequestDTO.setUsername(sendingUser.getUserProfile().getUsername());
+        friendRequestDTO.setUserId(receivingUserId);
+        friendRequestDTO.setUsername(receivingUser.getUserProfile().getUsername());
 
         Message feedbackMessage = new Message(MessageType.FEEDBACK, friendRequestDTO);
         simpMessagingTemplate.convertAndSend("/topic/" + sendingUserId + "/notifications", feedbackMessage);
